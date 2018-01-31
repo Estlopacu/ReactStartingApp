ec3d327935dbd849dba9be9d95d3a097a5146a50
Estlopacu <estlopacu@gmail.com>
2018-01-31 08:37:50 -0600

    Initial commit.

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Courses</div>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
